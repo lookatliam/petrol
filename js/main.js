2763eef5aa73044b550b9370b6438f6f0d5aa57b
liam <lookatliam@gmail.com>
2016-05-21 15:19:19 +0300

    Complete Desctop version

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(function(){
+	$('.display').slides({
+		preload: true
+	});
+
+	fr = new FilmRoll({
+		container: '.special',
+		height: 250,
+		pager: false,
+		position: 'left',
+		scroll: false
+	});
+
+	$(".film_roll_prev").eq(0).appendTo(".special");
+	$(".film_roll_next").eq(0).appendTo(".special");
+
+	fr2 = new FilmRoll({
+		container: '.special2',
+		height: 250,
+		pager: false,
+		position: 'left',
+		scroll: false
+	});
+
+	$(".film_roll_prev").eq(1).appendTo(".special2");
+	$(".film_roll_next").eq(1).appendTo(".special2");
+});
+
