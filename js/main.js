ca3c04d530abf29f7ae80e96d72d87e2af2af567
liam <lookatliam@gmail.com>
2016-05-22 16:50:56 +0300

    Complete Adaptive Index Page

--- js/main.js
+++ js/main.js
@@ -27,3 +27,20 @@
 	$(".film_roll_prev").eq(1).appendTo(".special2");
 	$(".film_roll_next").eq(1).appendTo(".special2");
 
+function capPosition () {
+	var sc = $('.slides_container').width();
+	var cap = $('.caption').width();
+	var capLength = $('.caption').length;
+	var r;
+
+	for(var i = 0; i < capLength; i++) {
+		r = (sc - cap)/2;
+		$('.caption').eq(i).css("left", r);
+	}
+}
+
+capPosition();
+
+$( window ).resize(function() {
+  capPosition();
+});
